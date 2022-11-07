af5d3da6b65090deee5855dd77409154b590281b
AlexseyDedul <dedyul@gmail.com>
2022-11-07 23:39:13 +0300

    complete unit 1

--- test_task/settings.py
+++ test_task/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,11 +83,11 @@ WSGI_APPLICATION = 'test_task.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-          'NAME': 'rocketdata',
-          'USER': 'alexsey',
-          'PASSWORD': 'postgres',
-          'HOST': 'localhost',
-          'PORT': '5432',
+          'NAME': env('NAME'),
+          'USER': env('USER'),
+          'PASSWORD': env('PASSWORD'),
+          'HOST': env('HOST'),
+          'PORT': env('PORT'),
     }
 }
 
